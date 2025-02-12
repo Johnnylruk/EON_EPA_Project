cd85c0e1ee3eca772b6685e58dd7625135d20a54
Johnny Rodrigues <johnny.rodrigues@fdmgroup.com>
2025-02-12 16:13:31 +0000

    Modifying file for classes done

--- removing_labels.py
+++ removing_labels.py
@@ -1,28 +1,43 @@
+import os
 # read the file content
 # transform the file content
 # then rewrite it the way i want 
 
-file_path = 'file_text.txt'
+base_path = 'labels'
+dir_list = os.listdir(base_path)
+
 keep_classl_list = ['2','11','8','1']
 
-# This is for reading file
-with open(file_path, 'r') as file:
-    read_file = file.read()
 
-#This for transforming the file
-split_file = read_file.split('\n')
-new_file = []
-for line in split_file:
-    split_line = line.split()
-    if split_line and split_line[0] in keep_classl_list:
-        new_file.append(line)
+for file_name in dir_list:
+    file_path = os.path.join(base_path, file_name)
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as file:
+            read_file = file.read()
 
-string_empty = ''
+    split_file = read_file.split('\n')
+    new_file = []
+    for line in split_file:
+        split_line = line.split()
+        if split_line and split_line[0] in keep_classl_list:
+            new_file.append(line)
 
-for st in new_file:
-    string_empty += st + '\n'
+    string_empty = ''
+
+    for st in new_file:
+        string_empty += st + '\n'
+
+    directory = os.path.join(base_path, 'transformed_files')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    save_location = os.path.join(directory, f'{file_name}')
+    with open(save_location, 'w') as file:
+        file.write(string_empty)
     
 
-with open('Removed_classe.txt', 'w') as file:
-    file.write(string_empty)
+
+
+
+
+
 
