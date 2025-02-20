be7428991b7ecebde8100f07221a92be09b44002
Johnny Rodrigues <johnny.rodrigues@fdmgroup.com>
2025-02-20 12:25:04 +0000

    added decode and save images process

--- roboflow_workflow_pipeline.py
+++ roboflow_workflow_pipeline.py
@@ -1,6 +1,9 @@
 from inference_sdk import InferenceHTTPClient
 from roboflow_connection import roboflow_connection_utility, get_roboflow_workflow_name_list
 from roboflow_workflow_pipeline_rules import workflow_rules_dict
+import base64
+from PIL import Image
+from io import BytesIO
 
 roboflow_connection = roboflow_connection_utility()
 
@@ -32,13 +35,30 @@ def run_roboflow_workflow(roboflow_client: InferenceHTTPClient, workflow_rules_d
 
     return workflow
 
-run_roboflow_workflow(
+def decode_images_from_string_format(workflow_output) -> list:
+    images_list_to_decode = []
+    for item in workflow_output:
+        for image in item:
+            if image == 'output_image':
+                images_list_to_decode.append(base64.b64decode(item[image]))
+    return images_list_to_decode
+
+def save_images_after_decode(decoded_image_list: list):
+
+    for pos, image in enumerate(decoded_image_list):
+        open_image = Image.open(BytesIO(image))
+        print(open_image)
+        open_image.save(f'image_{pos + 1}_with_label.jpg')
+
+images_list = ['PPE_Test_Image.jpg','PPE_Test_Image_2.jpg'] # Remind to myself to change it to a path
+
+result = run_roboflow_workflow(
     roboflow_client, 
     workflow_rules_dict,
     roboflow_connection['connection_response'],
     roboflow_connection['workspace_name'],
-    'PPE_Test_Image.jpg'
+    images_list
     )
 
-
-
+imagem_to_save = decode_images_from_string_format(result)
+save_images_after_decode(imagem_to_save)
